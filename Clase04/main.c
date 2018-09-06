08f581de354307a6e3a865c7235b1dcdcb02eecc
GuirlandNico <40776505+GuirlandNico@users.noreply.github.com>
2018-09-06 19:26:56 -0300

    Add files via upload

---
+++ Clase04/main.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "utn.h"
+
+
+int main()
+{
+
+
+    int edad;
+    //if(getEdad(&edad) == 0)
+    if(utn_getEntero(&edad,3,"edad?:","error",0,199) == 0)
+    {
+        printf("La edad es: %d",edad);
+    }
+
+
+
+    return 0;
+}
+
+
+
+
+
